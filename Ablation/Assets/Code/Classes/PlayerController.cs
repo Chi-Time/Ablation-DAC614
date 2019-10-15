f0e8d5d98db417620e389b011aeeef8152a1ef28
James Johnson <music1384@yahoo.co.uk>
2019-10-15 12:46:54 +0100

    Added Aura

---
+++ Ablation/Assets/Code/Classes/PlayerController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class PlayerController : MonoBehaviour
+{
+    private float _MoveSpeed = 0.0f;
+}
